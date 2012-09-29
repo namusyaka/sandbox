eabea598f9ffc6aab79c5b01e14862e8046ce43d
namusyaka <namusyaka@gmail.com>
2012-09-29 19:43:58 +0900

    Added a byproduct.

---
+++ sub-html-collection.js
@@ -0,0 +1,16 @@
+var SubHTMLCollection = function (collection) {
+  this.collection = Array.slice(collection);
+  return this;
+};
+
+SubHTMLCollection.prototype = {
+  each : function (callback) {
+    for(var i = 0, element; element = this.collection[i]; ++i)
+      callback.call(this, element, i);
+  },
+
+  reverseEach : function (callback) {
+    for(var i = this.collection.length - 1, element; element = this.collection[i]; --i)
+      callback.call(this, element, i);
+  }
+};
